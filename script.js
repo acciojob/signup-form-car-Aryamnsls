95881d09938e11e93a67f501828abdc246886552
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2025-03-19 01:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your code here
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (event) {
+        if (!document.getElementById("tos").checked) {
+            alert("You must agree to the Terms of Service to proceed.");
+            event.preventDefault();
+        }
+    });
+});
